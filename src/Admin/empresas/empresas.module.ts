f4b842175dcbf79a8ccb503bbdabc661c2d944a2
luis fonseca <kakaroto1500@gmail.com>
2022-06-03 08:56:10 -0300

    feat :organizando modulos internos

--- src/Admin/empresas/empresas.module.ts
+++ src/Admin/empresas/empresas.module.ts
@@ -8,6 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [TypeOrmModule.forFeature([EmpresaEntity])],
   controllers: [EmpresasController],
   providers: [EmpresasService],
+  exports: [EmpresasService]
 
 
 
